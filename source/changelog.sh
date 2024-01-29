9a5071363b8e2cc200db028b1098875245795b5b
Deepak Nadig <deepnadig@gmail.com>
2024-01-28 20:34:30 -0500

    Updated Changelog.

--- source/changelog.sh
+++ source/changelog.sh
@@ -7,5 +7,5 @@ Changelog
 EOF
 
 #git --no-pager log --pretty=format:'%d --- %h **%s** --- On (%cs) --- By <*%an*>%n%n' >> main/history.rst
-git --no-pager log --pretty=format:'Commit: %h --- **%s** --- On (%cs) --- By <*%an*>%n%n' >> main/history.rst
+git --no-pager log --pretty=format:'Commit: %h --- **%s** --- On (%cs) --- By <*%an*>%n' >> main/history.rst
 echo
